2a749ae0fe92ed75b47396ae1204e15d167ba1a2
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2022-06-07 19:46:42 +0530

    Password-Change

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require("./module/module");
 const server = http.createServer(app)
 
 
-server.listen(3002, () => {
+server.listen(process.env.PORT||3002, () => {
     console.log(`Server running on port ${3002}`);
   });
